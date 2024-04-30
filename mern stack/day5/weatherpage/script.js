1ccc196ae9e685345c7f77cdb5c9784237adb920
Ekata Gupta <ekata8077@gmail.com>
2024-04-30 21:56:53 +0530

    Add files via upload

---
+++ mern stack/day5/weatherpage/script.js
@@ -0,0 +1,32 @@
+// function fetchAPI(){
+//     const  url ='https:weather.visualcrossing.FDLBV8WHVFLGN4JRNWEK7NP2R'
+//     fetch(url)
+//     .then((res)=>res,json())
+//     .then(console.log);
+
+// }
+
+// fetchAPI()
+
+
+// function renderUI(data){
+//     const rowm= document.createElement("tr");
+
+// let cell document.createElement("th");
+// const cell1.innertext='date';
+// rowm.appendchild(cell);
+
+// const cell2.innertext=("th");
+// cell2.innertext = 'maximum temperature';
+// childrw.appendchild(c2);
+// ShadowRoot.appendchild(childrow)
+
+
+
+// }
+
+
+let a = 5;
+let b = 10;
+let sum = a+b;
+console.log(sum);
